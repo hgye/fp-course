e0fb520446d2d06d13225c6fc43edfbc068a0313
hgye <greenar@gmail.com>
2018-09-19 10:12:09 +0000

    fastAnagrams finished

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -6,16 +6,30 @@ module Course.FastAnagrams where
 import Course.Core
 import Course.List
 import Course.Functor
+import Course.Applicative
 import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
+fastAnagrams' ::
+  Chars
+  -> FilePath
+  -> IO (List Chars)
+fastAnagrams' s fp =
+    (((<$>) filter)
+    (flip S.member <$>  ((S.fromList . hlist . lines) <$> readFile fp)))
+    <*>
+    (pure $ permutations s)
+  -- error "todo: Course.FastAnagrams#fastAnagrams"
 fastAnagrams ::
   Chars
   -> FilePath
   -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams  s fp =
+  (flip (filter . flip S.member))
+  (permutations s) <$>
+  ((S.fromList . hlist . lines) <$> readFile fp)
+
 
 newtype NoCaseString =
   NoCaseString {
